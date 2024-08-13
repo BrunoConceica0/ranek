0acf23639896f62801b6282c2ef6646024a32643
BrunoConceica0 <bd687353@gmail.com>
2024-08-13 20:53:47 -0300

    Header e Footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,64 @@
 <template>
   <div id="app">
-    <nav id="nav">
-      <router-link to="/">Home</router-link>
-    </nav>
+    <TheHeader />
+    <main id="main">
+      <router-view />
+    </main>
+    <TheFooter />
   </div>
 </template>
 
-<style></style>
+<script>
+import TheHeader from "@/components/TheHeader";
+import TheFooter from "@/components/TheFooter";
+
+export default {
+  components: { TheHeader, TheFooter },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  text-decoration: none;
+
+  color: #345;
+  background: url("./assets/ranek-assets/pattern.svg") repeat top;
+}
+a {
+  color: #345;
+}
+img {
+  max-width: 100%;
+  display: block;
+}
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background: #87f;
+  border-radius: 4px;
+  color: white;
+  text-align: center;
+  font-size: 1rem;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+  transition: all 0.3s;
+  border: none;
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #65d;
+  transform: scale(1.1);
+}
+#app {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+#main {
+  flex: 1;
+}
+</style>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <footer>
+      <p>Ranke. Alguns direitos reservados</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheFooter",
+};
+</script>
+
+<style scoped>
+footer {
+  background: #87f;
+  padding: 25px 0;
+  text-align: center;
+  font-size: 1.2rem;
+  position: relative;
+  top: 75px;
+}
+footer p {
+  color: #fff;
+}
+</style>
