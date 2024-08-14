d2139c7d3c094f5c5e74f4d1b4c2a40c56f25f0c
BrunoConceica0 <bd687353@gmail.com>
2024-08-13 22:40:21 -0300

    Adicionado busca de pesquisa e lista

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,11 +1,44 @@
 <template>
-  <p>Lista</p>
+  <section class="produtos-container">
+    <div>
+      <ul>
+        <li v-for="produto in produtos" :key="produto.id">
+          <img
+            v-if="produto.foto"
+            :src="produto.foto[0].scr"
+            :alt="produto.foto[0].titulo"
+          />
+          <p class="produto_preco">{{ produto.preco }}</p>
+          <h2 class="produto_nome">{{ produto.nome }}</h2>
+          <p>{{ produto.descricao }}</p>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      produtos: null,
+    };
+  },
+  methods: {
+    getProdutos() {
+      fetch("http://localhost:3000/produto").then((response) =>
+        response.json().then((data) => {
+          this.produtos = data;
+        })
+      );
+    },
+  },
+  created() {
+    this.getProdutos();
+  },
   name: "produtosLista",
 };
 </script>
 
 <style></style>
+http://localhost:3000/produto
